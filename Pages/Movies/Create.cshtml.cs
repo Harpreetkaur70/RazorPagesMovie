b95f546cf0a95f54665f9442b6965ebaf2ea0ee4
Harpreet Kaur <harpreetkaur11379@gmail.com>
2021-12-16 22:24:21 +0530

    First Commit

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie1;
+using RazorPagesMovie1.Models;
+
+namespace RazorPagesMovie1.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie1.RazorPagesMovieContext1 _context;
+
+        public CreateModel(RazorPagesMovie1.RazorPagesMovieContext1 context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesMovieContext1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie1
+{
+    public class RazorPagesMovieContext1:DbContext
+    {
+        public RazorPagesMovieContext1(
+            DbContextOptions<RazorPagesMovieContext1> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<RazorPagesMovie1.Models.Movie> Movie { get; set; }
+
+    }
+}
